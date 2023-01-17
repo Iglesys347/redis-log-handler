f258ef3aeef1cff1f90af994ccfd7680cf5a9597
Iglesys <g.imbert34@gmail.com>
2023-01-17 17:24:55 +0100

    Fix doc conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,42 +1,3 @@
-import os
-import sys
-
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-sys.path.append(os.path.abspath(os.path.pardir))
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'redis-log-handler'
-copyright = '2023, Iglesys347'
-author = 'Iglesys347'
-release = '1.0.0'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 # -- Options for HTML output -------------------------------------------------
 import os
 import sys
@@ -54,7 +15,7 @@ sys.path.append(os.path.abspath(os.path.pardir))
 project = 'redis-log-handler'
 copyright = '2023, Iglesys347'
 author = 'Iglesys347'
-release = '0.0.3-dev'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
